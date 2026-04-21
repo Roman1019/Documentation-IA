89e1ca46f150f0f8643ddabe7e856bb4ea2df27a
Roman1019 <142348028+Roman1019@users.noreply.github.com>
2026-04-21 23:15:53 +0300

    workflow-solution section

--- main.js
+++ main.js
@@ -1,21 +1,18 @@
-// Ініціалізація Swiper
 document.addEventListener("DOMContentLoaded", () => {
   const swiper = new Swiper(".feedback-swiper", {
     slidesPerView: 1,
     spaceBetween: 30,
 
-    // Навігація (твої кнопки)
     navigation: {
       nextEl: ".next",
       prevEl: ".prev",
     },
     pagination: {
       el: ".feedback-pagination",
-      clickable: true, // щоб можна було клікати по крапочках
+      clickable: true,
       dynamicBullets: true,
     },
 
-    // Адаптив
     breakpoints: {
       768: {
         slidesPerView: 2,
@@ -25,9 +22,49 @@ document.addEventListener("DOMContentLoaded", () => {
         slidesPerGroup: 2,
       },
     },
+    loop: true,
+    speed: 600,
+  });
+});
 
-    // Додатково (можеш включити якщо хочеш)
-    loop: true, // безкінечний скрол
-    speed: 600, // плавність
+// document.addEventListener("DOMContentLoaded", () => {
+//   const items = document.querySelectorAll(".advantages-accordion-item");
+
+//   items.forEach((item) => {
+//     const header = item.querySelector(".advantages-accordion-header");
+
+//     header.addEventListener("click", () => {
+//       const isActive = item.classList.contains("active");
+//       items.forEach((i) => i.classList.remove("active"));
+//       if (!isActive) {
+//         item.classList.add("active");
+//       }
+//     });
+//   });
+// });
+document.addEventListener("DOMContentLoaded", () => {
+  const items = document.querySelectorAll(".advantages-accordion-item");
+
+  items.forEach((item) => {
+    const header = item.querySelector(".advantages-accordion-header");
+    const content = item.querySelector(".advantages-accordion-content");
+    const line = item.querySelector(".line");
+
+    header.addEventListener("click", () => {
+      items.forEach((i) => {
+        i.classList.remove("active");
+
+        const c = i.querySelector(".advantages-accordion-content");
+        const l = i.querySelector(".line");
+
+        c.style.maxHeight = null;
+        l.style.height = "40px"; // повертаємо маленьку
+      });
+
+      item.classList.add("active");
+
+      content.style.maxHeight = content.scrollHeight + "px";
+      line.style.height = content.scrollHeight + 40 + "px";
+    });
   });
 });
